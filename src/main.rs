3b6050a11d3dd6b7ce72ea65a102769aac0964d4
Talison Fabio <54823205+talis-fb@users.noreply.github.com>
2024-03-25 20:03:37 -0300

    add cli

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,49 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use image::{GenericImageView, Pixel};
 use std::error::Error;
 use std::path::PathBuf;
 
 use std::io::stdout;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(name = "catimg", author, version, about, long_about = None)] // Get info in Cargo.toml
 pub struct Args {
-    #[command()]
-    pub file: Option<PathBuf>,
+    #[command(subcommand)]
+    command: SubCommands,
 }
 
-// TODO: Make the widht be first
-// = width x height
+#[derive(Subcommand)]
+pub enum SubCommands {
+    ToImage {
+        file: PathBuf,
+
+        #[arg(short, long)]
+        output_file: PathBuf,
+    },
+    ToCsv {
+        file: PathBuf,
+    } 
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = Args::parse();
-    let path = matches.file.unwrap();
 
+    match matches.command {
+        SubCommands::ToCsv { file } => {
+            image_to_csv(file)?;
+        }
+        SubCommands::ToImage { file, output_file } => {
+            csv_to_image(file, output_file)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn csv_to_image(file: PathBuf, output_file: PathBuf) -> Result<(), Box<dyn Error>> {
     let mut rdr = csv::ReaderBuilder::new()
         .delimiter(b' ')
-        .from_path(path)
+        .from_path(file)
         .unwrap();
 
     let mut pixels: Vec<((u32, u32), u8, u8, u8)> = Vec::new();
@@ -47,56 +69,53 @@ fn main() -> Result<(), Box<dyn Error>> {
         img.put_pixel(y, x, image::Rgb([red, green, blue]));
     }
 
-    img.save("output_do_meu_cli.png").unwrap();
+    img.save(output_file).unwrap();
 
     Ok(())
 }
 
-// fn main() -> Result<(), ()> {
-//     let matches = Args::parse();
-//     let path = matches.file.unwrap();
-//
-//     let img = image::open(path).unwrap();
-//     let (width, height) = img.dimensions();
-//
-//     struct Pixel {
-//         red: u8,
-//         green: u8,
-//         blue: u8,
-//         position: (u32, u32),
-//     }
-//
-//     let mut values: Vec<Pixel> = Vec::default();
-//
-//     for row in 0..height {
-//         for col in 0..width {
-//             let pixel = img.get_pixel(col, row);
-//             let red = pixel[0];
-//             let green = pixel[1];
-//             let blue = pixel[2];
-//             values.push(Pixel {
-//                 red,
-//                 green,
-//                 blue,
-//                 position: (row, col),
-//             });
-//         }
-//     }
-//
-//     let mut csv_writer = csv::WriterBuilder::new()
-//         .delimiter(b' ')
-//         .from_writer(stdout());
-//
-//     for v in values {
-//         csv_writer
-//             .write_record(&[
-//                 format!("{}:{}", v.position.0, v.position.1),
-//                 v.red.to_string(),
-//                 v.green.to_string(),
-//                 v.blue.to_string(),
-//             ])
-//             .unwrap();
-//     }
-//
-//     Ok(())
-// }
+fn image_to_csv(file: PathBuf) -> Result<(), Box<dyn Error>> {
+    let img = image::open(file).unwrap();
+    let (width, height) = img.dimensions();
+
+    struct Pixel {
+        red: u8,
+        green: u8,
+        blue: u8,
+        position: (u32, u32),
+    }
+
+    let mut values: Vec<Pixel> = Vec::default();
+
+    for row in 0..height {
+        for col in 0..width {
+            let pixel = img.get_pixel(col, row);
+            let red = pixel[0];
+            let green = pixel[1];
+            let blue = pixel[2];
+            values.push(Pixel {
+                red,
+                green,
+                blue,
+                position: (row, col),
+            });
+        }
+    }
+
+    let mut csv_writer = csv::WriterBuilder::new()
+        .delimiter(b' ')
+        .from_writer(stdout());
+
+    for v in values {
+        csv_writer
+            .write_record(&[
+                format!("{}:{}", v.position.0, v.position.1),
+                v.red.to_string(),
+                v.green.to_string(),
+                v.blue.to_string(),
+            ])
+            .unwrap();
+    }
+
+    Ok(())
+}
